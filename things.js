fde7285b25ecce6200043266ae2a197f5eb0e498
IceScout26 <dhimasscout@gmail.com>
2023-10-07 15:48:38 +0700

    chore : error handling

--- things.js
+++ things.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 var pool = require('./queries.js');
 
+//error handling
+function handleQueryError(err, res) {
+    console.error(err); // Log error ke konsol untuk debugging
+    res.status(500).json({ error: 'Internal Server Error' }); // Respond dengan pesan error
+}
+
 // This route for showing all films
 router.get('/films', function (req, res) {
     pool.query('SELECT * FROM film', (err, result) => {
         if (err) {
-            console.error(err); 
-            res.status(500).json({ error: 'Internal Server Error' }); // Respond with an error message
+            handleQueryError(err, res);
         } else {
             res.json(result.rows); // Send the data as JSON
         }
@@ -19,8 +24,7 @@ router.get('/films/:film_id', function (req, res) {
     const filmId = req.params.film_id; // get film_id from request params
     pool.query('SELECT * FROM film WHERE film_id = $1', [filmId], (err, result) => {
         if (err) {
-            console.error(err); 
-            res.status(500).json({ error: 'Internal Server Error' });
+            handleQueryError(err, res);
         } else {
             if (result.rows.length === 0) {
                 res.status(404).json({ error: 'Film tidak ditemukan' }); // Respon if no data
@@ -36,8 +40,7 @@ router.get('/films/:film_id', function (req, res) {
 router.get('/categories', function (req, res) {
     pool.query('SELECT * FROM category', (err, result) => {
         if (err) {
-            console.error(err); 
-            res.status(500).json({ error: 'Internal Server Error' }); // Respond with an error message
+            handleQueryError(err, res);
         } else {
             res.json(result.rows); // Send the data as JSON
         }
@@ -55,8 +58,7 @@ router.get('/films/category/:categoryName', function (req, res) {
         [categoryName],
         (err, result) => {
             if (err) {
-                console.error(err); 
-                res.status(500).json({ error: 'Internal Server Error' }); // Respond with an error message
+                handleQueryError(err, res);
             } else {
                 res.json(result.rows);
             }
