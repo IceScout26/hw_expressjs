a4d73e406a325f0af1aaa56ab1739b7a8b4deee5
IceScout26 <dhimasscout@gmail.com>
2023-10-07 13:51:35 +0700

    feat : seeding 5 data table actor

---
+++ db/seeding.sql
@@ -0,0 +1,7 @@
+INSERT INTO actor (first_name, last_name, last_update)
+VALUES
+    ('John', 'Doe', NOW()),
+    ('Jane', 'Smith', NOW()),
+    ('Michael', 'Johnson', NOW()),
+    ('Emily', 'Williams', NOW()),
+    ('David', 'Brown', NOW());
